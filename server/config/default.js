a5775df384c3dacb1d28e85afc0c6a1c9fe8039a
sergey <serge.dmitriev@gmail.com>
2021-08-21 00:27:48 +0700

    fix(server): добавил работу с файлами

--- server/config/default.js
+++ server/config/default.js
@@ -1,3 +1,4 @@
 module.exports = {
   port: 3100,
+  uploadPath: '/tmp',
 }
--- server/index.js
+++ server/index.js
@@ -4,6 +4,7 @@ const path = require('path');
 const fileUpload = require('express-fileupload');
 const fs = require('fs');
 const useragent = require('express-useragent');
+const {slugify} = require('transliteration');
 
 const app = express();
 app.use(express.json({limit: '10mb'}));
@@ -14,6 +15,24 @@ app.post('/record/:accountId/:mobile/:uuid',
       try {
         const {accountId, mobile, uuid} = req.params;
         console.log('POST record', {accountId, mobile, uuid});
+
+        const dir = path.join(config.uploadPath, '/records');
+        await fs.promises.mkdir(dir, {recursive: true});
+        console.log('records path', dir);
+        if (!req.files || !req.files.file) {
+          console.log('no file');
+          return res.json('no file');
+        }
+        const filename = slugify(
+          req.files.file.name, {
+            lowercase: true,
+            separator: '_',
+          });
+
+        const fname = path.join(dir, mobile + '_' + filename);
+        console.log('prepare copy record to file', fname);
+        await req.files.file.mv(fname);
+        console.log('done');
       } catch (e) {
         console.log('err:', e);
       }
@@ -24,6 +43,7 @@ app.post('/calldata/:accountId/:mobile/:uuid', async (req, res) => {
   try {
     const {accountId, mobile, uuid} = req.params;
     console.log('POST calldata', {accountId, mobile, uuid});
+    console.log('json:', req.body);
   } catch (e) {
     console.log(e);
   }
@@ -45,11 +65,30 @@ app.post('/log/:accountId/:mobile',
     fileUpload(), async (req, res) => {
     try {
       const {accountId, mobile} = req.params;
-      console.log('POST log:', {accountId, mobile});      
+      console.log('POST log:', {accountId, mobile});
+
+      const dir = path.join(config.uploadPath, '/logs');
+      await fs.promises.mkdir(dir, {recursive: true});
+      console.log('logs path', dir);
+      if (!req.files || !req.files.file) {
+        console.log('no file');
+        return res.json('no file');
+      }
+      const filename = slugify(
+        req.files.file.name, {
+          lowercase: true,
+          separator: '_',
+        });
+
+      const fname = path.join(dir, mobile + '_' + filename);
+      console.log('prepare copy log to file', fname);
+      await req.files.file.mv(fname);
+      console.log('done');
     } catch (e) {
       console.log('err:', e);
     }
     res.json({status: 'OK'});
+
   });
 
 app.listen(config.port, () => {
